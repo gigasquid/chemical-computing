8a8aa6d34f6d0ac2f905eeb32936fc626c0f2444
gigasquid <cmeier@gigasquidsoftware.com>
2015-06-16 08:32:37 -0400

    basically working

--- dining-philosophers/src/chemical_computing/core.cljs
+++ dining-philosophers/src/chemical_computing/core.cljs
@@ -122,37 +122,43 @@
   (let [react-fn-args-list  (.-length react-fn)]
     (= react-fn-args-list (count arglist))))
 
+(declare gen-fork-molecule)
+(declare gen-eat-philosopher-molecule)
+(declare gen-think-philosopher-molecule)
+
+(defn gen-molecule-by-val [val x]
+  (case val
+    "two-forks and thinking-philosopher" [(gen-fork-molecule (- x 25) 450)
+                                          (gen-fork-molecule (+ x 25) 450)
+                                          (gen-think-philosopher-molecule x 450)]
+    "thinking-philosopher" (gen-think-philosopher-molecule x 450)
+    "eating-philosopher" (gen-eat-philosopher-molecule x 450)))
+
 (defn higher-order-eval [fn-mol]
   (let [react-fn (:val fn-mol)
         react-args (:args fn-mol)
         result-vals (apply react-fn react-args)
-        result-mols (mapv #(gen-molecule %) result-vals)]
-    (mapv #(assoc % :x (+ (:d fn-mol) (:x fn-mol)) :y (+ (:d fn-mol) (:y fn-mol))) result-mols)))
+        result-mols (flatten (mapv #(gen-molecule-by-val % (:x fn-mol)) result-vals))]
+    result-mols))
 
 (defn higher-order-capture [fn-mol val-mol]
   (let [react-fn-args (:args fn-mol)
-        react-fn (:val fn-mol)]
-    (if (react-fn-ready-to-eval? react-fn react-fn-args)
-      [fn-mol val-mol]
-      [(assoc fn-mol :args (conj react-fn-args (:val val-mol)))
-       (assoc val-mol :val :destroy)])))
+        react-fn (:val fn-mol)
+        react-allowed-arg-val (:allowed-arg-val fn-mol)]
+    (if (= react-allowed-arg-val (:val val-mol))
+        (if (react-fn-ready-to-eval? react-fn react-fn-args)
+          [fn-mol val-mol]
+          [(assoc fn-mol :args (conj react-fn-args val-mol))
+           (assoc val-mol :val :destroy)])
+        [fn-mol val-mol])))
 
-(defn higher-order-arg-exchange [fn-mol1 fn-mol2]
-  (let [fn-args-1-want (.-length (:val fn-mol1))
-        fn-args-1-has (count (:args fn-mol1))
-        will-take (- fn-args-1-want fn-args-1-has)
-        fn-args-2 (:args fn-mol2)
-        new-fn-args-1 (into [] (concat (:args fn-mol1) (take will-take fn-args-2)))
-        new-fn-args-2 (into [] (drop will-take fn-args-2))]
-    [(assoc fn-mol1 :args new-fn-args-1)
-     (assoc fn-mol2 :args new-fn-args-2)]))
 
 (defn higher-order-reaction [mol1 mol2]
   (let [v1 (:val mol1)
         v2 (:val mol2)]
     (cond
       (and (fn? v1) (fn? v2))
-      (higher-order-arg-exchange mol1 mol2)
+      [mol1 mol2]
 
       (fn? v1)
       (higher-order-capture mol1 mol2)
@@ -245,11 +251,24 @@
     [a b]))
 
 
-(defn eat [f1 f2]
-  ["EP"])
+(defn get-forks [tp]
+  (let [diam (* 2 (:d tp))
+        rest-molecules (remove (fn [b] (= (:id tp) (:id b))) (vals @world))
+        collided-with (filter (fn [b] (and (= "f" (:val b))
+                                          (collide? b (:x tp) (:y tp) diam))) rest-molecules)]
+    collided-with))
 
-(defn think [ep]
-  ["f" "f"])
+(defn eat [mol]
+  (let [forks (get-forks mol)]
+    (if (= 2 (count forks))
+      (let [[f1 f2] forks]
+        (swap! world dissoc (:id f1) (:id f2))
+        ["eating-philosopher"])
+      ["thinking-philosopher"])))
+
+(defn think [mol]
+ ["two-forks and thinking-philosopher"])
+
 
 (defn gen-fork-molecule [x y]
   {:id (swap! mol-id-counter inc)
@@ -262,6 +281,28 @@
    :dy 0.0
    :args []})
 
+(defn gen-think-philosopher-molecule [x y]
+  {:id (swap! mol-id-counter inc)
+   :x x
+   :y y
+   :d 20
+   :val "TP"
+   :color "lightblue"
+   :dx 0.0
+   :dy 0.0
+   :args []})
+
+(defn gen-eat-philosopher-molecule [x y]
+  {:id (swap! mol-id-counter inc)
+   :x x
+   :y y
+   :d 20
+   :val "EP"
+   :color "yellow"
+   :dx 0.0
+   :dy 0.0
+   :args []})
+
 (defn gen-eat-molecule [x y]
   {:id (swap! mol-id-counter inc)
    :x x
@@ -270,8 +311,9 @@
    :val eat
    :color "lightgreen"
    :dx 0.0
-   :dy (* (+ 0.5 (rand-int 3)) (rand-dx-dy))
-   :args []})
+   :dy (+ (rand) 1)
+   :args []
+   :allowed-arg-val "TP"})
 
 (defn gen-think-molecule [x y]
   {:id (swap! mol-id-counter inc)
@@ -281,14 +323,16 @@
    :val think
    :color "orange"
    :dx 0.0
-   :dy (* (+ 0.5 (rand-int 3)) (rand-dx-dy))
-   :args []})
+   :dy (- (rand) -1)
+   :args []
+   :allowed-arg-val "EP"})
 
 
 
 
 (def example-primes-mols (concat
                           (mapv gen-fork-molecule (range 25 500 50) (repeat 450))
+                          (mapv gen-think-philosopher-molecule (range 50 500 50) (repeat 450))
                           (mapv gen-eat-molecule  (range 50 500 50) (repeat 300))
                           (mapv gen-think-molecule (range 50 500 50) (repeat 100))
                           ))
